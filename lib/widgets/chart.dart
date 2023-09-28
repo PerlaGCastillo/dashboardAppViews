6420c5ff713dab4394ea8c789dd9cdc081a0c8ff
perla_godinez <perla.godinez@stps.gob.mx>
2023-09-28 10:58:39 -0600

    restoring

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -4,8 +4,8 @@ import 'package:dgapd/data/utlity.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Chart extends StatefulWidget {
-  int index;
-  Chart({Key? key, required this.index}) : super(key: key);
+  int indexx;
+  Chart({Key? key, required this.indexx}) : super(key: key);
 
   @override
   State<Chart> createState() => _ChartState();
@@ -17,7 +17,7 @@ class _ChartState extends State<Chart> {
   bool j = true;
   @override
   Widget build(BuildContext context) {
-    switch (widget.index) {
+    switch (widget.indexx) {
       case 0:
         a = today();
         b = true;
@@ -54,17 +54,17 @@ class _ChartState extends State<Chart> {
                 return SalesData(
                     j
                         ? b
-                            ? a![index].datetime.hour.toString()
-                            : a![index].datetime.day.toString()
+                        ? a![index].datetime.hour.toString()
+                        : a![index].datetime.day.toString()
                         : a![index].datetime.month.toString(),
                     b
                         ? index > 0
-                            ? time(a!, true)[index] + time(a!, true)[index - 1]
-                            : time(a!, true)[index]
+                        ? time(a!, true)[index] + time(a!, true)[index - 1]
+                        : time(a!, true)[index]
                         : index > 0
-                            ? time(a!, false)[index] +
-                                time(a!, false)[index - 1]
-                            : time(a!, false)[index]);
+                        ? time(a!, false)[index] +
+                        time(a!, false)[index - 1]
+                        : time(a!, false)[index]);
               })
             ],
             xValueMapper: (SalesData sales, _) => sales.year,
