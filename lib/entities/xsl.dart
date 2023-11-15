8d70d346781b09ff0ed791fe60ca1b84cc47b60f
perla_godinez <perla.godinez@stps.gob.mx>
2023-11-15 13:42:35 -0600

    xls in progress

--- lib/entities/xsl.dart
+++ lib/entities/xsl.dart
@@ -32,6 +32,28 @@ class xlsConverter extends StatelessWidget {
   }
 }
 
+
+
+/**/
+
+Future<String> get _localPath async{
+  final directory = await getApplicationDocumentsDirectory();
+  return directory.path;
+}
+
+Future<File> get _localFile async{
+  final path = await _localPath;
+  return File('$path/filename.xlsx');
+}
+
+Future<File> writeCounter(Excel excel) async{
+  final file = await _localFile;
+  return file.writeAsBytes(excel.encode());
+}
+
+/**/
+
+
 Widget _createExcel {
   Future<void> _createExcel() async {
 
